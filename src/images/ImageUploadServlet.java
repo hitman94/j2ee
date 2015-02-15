4c1ef101e57356d6ecfd317c9411d5179015f356
Thomas Guerin <adminmizix@gmail.com>
2015-02-15 22:51:17 +0100

    Upload Images ajax

---
+++ src/images/ImageUploadServlet.java
@@ -0,0 +1,54 @@
+package images;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class ImageUploadServlet
+ */
+@WebServlet("/ImageUploadServlet")
+@MultipartConfig
+public class ImageUploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ImageUploadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Part p = request.getPart("imageLocation");
+		File f = new File(p.getSubmittedFileName());
+		if(f.mkdir()) {
+			response.setStatus(200);
+			response.getWriter().write("<html><head></head><body>ok</body></html>");
+			return;
+		}
+		
+		response.setStatus(400);
+		response.getWriter().write("non");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
